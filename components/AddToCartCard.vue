e978d1474c12f4eb0a8ae797dbb6638ab8f467be
verma-abhishek-demo <abhishek123vrma@gmail.com>
2024-09-25 22:14:35 +0530

    edited radio button on course-info page

--- components/AddToCartCard.vue
+++ components/AddToCartCard.vue
@@ -15,6 +15,12 @@ const courseDurationType = ([
     { id: 2, title: '6', salePrice: '890', originalPrice: '1280' },
     { id: 3, title: '12', salePrice: '1280', originalPrice: '1590' }
 ])
+
+watch(courseDuration, () => {
+    // Reset to default state whenever the user selects a different radio button
+    cartAdd.value = false;
+    cartUpdated.value = false;
+});
 </script>
 
 <template>
@@ -27,8 +33,10 @@ const courseDurationType = ([
             <p class="add_cart_title_p">Course validity</p>
             <div class="duration_price_div">
                 <div v-for="(item, index) in courseDurationType" :key="index" class="d-flex align-center duration_mt justify-between">
-                    <label class="d-flex align-center">
-                        <input class="duration_radio" type="radio" />
+                    <label class="radio_container d-flex align-center">
+                        
+                        <input v-model="courseDuration" :value="item.id" class="duration_radio" type="radio" />
+                        <span class="duration_radio_span checkmark"></span>
                         <p class="duration_month">{{ item.title }} Month</p>
                     </label>
                     <div class="d-flex align-center">
@@ -37,7 +45,7 @@ const courseDurationType = ([
                     </div>
                 </div>
             </div>
-            <button @click="addToCart" v-if="cartAdd == false && cartUpdated == false" class="add_cart_btn">Add To Cart</button>
+            <button @click="addToCart" :disabled="!courseDuration" v-if="cartAdd == false && cartUpdated == false" :class="{'disabled-button': !courseDuration}" class="add_cart_btn">Add To Cart</button>
             <button v-if="cartAdd == true" class="add_cart_btn d-flex justify-center">
                 <img src="/images/cart_loader.png" />
             </button>
@@ -47,14 +55,50 @@ const courseDurationType = ([
 </template>
 
 <style scoped>
+.radio_container {
+    position: relative;
+}
+.radio_container input {
+    position: absolute;
+    top: 3px;
+    left: 0;
+    opacity: 0;
+}
+.radio_container input:checked ~ .checkmark {
+    background-color: #F4CCB5;
+}
+.disabled-button {
+    cursor: not-allowed;
+    opacity: 0.6;
+}
 .checkmark {
     position: absolute;
-    top: 0;
+    top: 3px;
     left: 0;
-    height: 9px;
-    width: 9px;
+    height: 12px;
+    width: 12px;
     background-color: #F4CCB5;
     border-radius: 50%;
+    border: 1px solid #757373;
+}
+
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.radio_container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+.radio_container .checkmark:after {
+ 	top: 2px;
+	left: 1px;
+	width: 7px;
+	height: 7px;
+	border-radius: 50%;
+	background: #F87126;
 }
 
 .add_cart_btn {
@@ -79,7 +123,7 @@ const courseDurationType = ([
 
 p.duration_month {
     font-size: 13px;
-    margin-left: 6px !important;
+    margin-left: 18px !important;
     font-weight: 500;
     color: #000;
 }
