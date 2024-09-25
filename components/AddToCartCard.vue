54555988a1336470b504263e4bdb2b254b919bdd
eligocs <38901899+eligocs@users.noreply.github.com>
2024-09-25 18:59:35 +0530

    course info page done

--- components/AddToCartCard.vue
+++ components/AddToCartCard.vue
@@ -10,6 +10,11 @@ const addToCart = () => {
         cartUpdated.value = true;
     }, 2000)
 }
+const courseDurationType = ([
+    { id: 1, title: '3', salePrice: '399', originalPrice: '580' },
+    { id: 2, title: '6', salePrice: '890', originalPrice: '1280' },
+    { id: 3, title: '12', salePrice: '1280', originalPrice: '1590' }
+])
 </script>
 
 <template>
@@ -21,36 +26,14 @@ const addToCart = () => {
             <h1 class="add_cart_title_h">Early Math Skills Learning Numbers and Shapes</h1>
             <p class="add_cart_title_p">Course validity</p>
             <div class="duration_price_div">
-                <div class="d-flex align-center duration_mt justify-between">
+                <div v-for="(item, index) in courseDurationType" :key="index" class="d-flex align-center duration_mt justify-between">
                     <label class="d-flex align-center">
                         <input class="duration_radio" type="radio" />
-                        <p class="duration_month">3 Month</p>
+                        <p class="duration_month">{{ item.title }} Month</p>
                     </label>
                     <div class="d-flex align-center">
-                        <p class="duration_sp">₹399 /-</p>
-                        <p class="duration_op">₹580</p>
-                    </div>
-                </div>
-
-                <div class="d-flex align-center duration_mt justify-between">
-                    <div class="d-flex align-center">
-                        <input class="duration_radio" type="radio" />
-                        <p class="duration_month">6 Month</p>
-                    </div>
-                    <div class="d-flex align-center">
-                        <p class="duration_sp">₹890 /-</p>
-                        <p class="duration_op">₹1280</p>
-                    </div>
-                </div>
-
-                <div class="d-flex align-center duration_mt justify-between">
-                    <div class="d-flex align-center">
-                        <input class="duration_radio" type="radio" />
-                        <p class="duration_month">12 Month</p>
-                    </div>
-                    <div class="d-flex align-center">
-                        <p class="duration_sp">₹1250 /-</p>
-                        <p class="duration_op">₹1590</p>
+                        <p class="duration_sp">₹{{ item.salePrice }} /-</p>
+                        <p class="duration_op">₹{{ item.originalPrice }}</p>
                     </div>
                 </div>
             </div>
